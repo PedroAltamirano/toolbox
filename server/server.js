92f9b5a6aa433fba9bbf4b87268c823fb37fb7ab
PedroAltamirano <pedroaal04@gmail.com>
2021-12-04 09:45:10 -0500

    added palindrome check

--- server/server.js
+++ server/server.js
@@ -1,27 +1,31 @@
 const express = require('express')
 const app = express()
-const cors = require("cors");
+const cors = require('cors')
 const port = 3000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 const reverseWords = str => {
-  return str.split(" ")
+  return str.split(' ')
     .map(word => {
       return word
-        .split("")
+        .split('')
         .reverse()
-        .join("");
+        .join('')
     })
-    .join(" ");
-};
+    .join(' ')
+}
+
+const isPalindrome = word => {
+  const reverse = reverseWords(word)
+  return word === reverse
+}
 
 app.get('/api/iecho', (req, res) => {
   const query = req.query.text
-  console.log(typeof query)
-  if (!query || query === '' || typeof query !== 'string') res.json({ 'error': 'no text' })
-  res.json({ 'text': reverseWords(query) })
+  if (!query || query === '' || typeof query !== 'string') res.json({ error: 'no text' })
+  res.json({ text: reverseWords(query), palindrome: isPalindrome(query) })
 })
 
 app.listen(port, () => {
